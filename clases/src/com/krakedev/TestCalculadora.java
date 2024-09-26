fe5d5ea3dd8217a867ddf4c1284a890e795e190f
Gulum72 <freiresteven72@gmail.com>
2024-09-25 22:42:48 -0500

    multi,divi,mensaje terminados

--- clases/src/com/krakedev/TestCalculadora.java
+++ clases/src/com/krakedev/TestCalculadora.java
@@ -6,13 +6,25 @@ public class TestCalculadora {
 		Calculadora calcu;
 		int resultadoSumaa;
 		int resultadoResta;
+		double resultadoMultiplicacion;
+		double resultadoDivision;
+		String mostrarResultado;
 		calcu=new Calculadora();
 		resultadoSumaa=calcu.sumar(5, 3);
-		resultadoResta=calcu.resta(10, 3);
+		resultadoResta=calcu.restar(10, 3);
+		resultadoMultiplicacion=calcu.multiplicar(4, 3);
+		resultadoDivision=calcu.dividir(6, 2);
+		
 		
 		System.out.println("RESULTADO SUMA: "+resultadoSumaa);
 		System.out.println("----------------------------");
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
+		System.out.println("----------------------------");
+		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicacion);
+		System.out.println("----------------------------");
+		System.out.println("RESULTADO DIVISION: "+resultadoDivision);
+		System.out.println("----------------------------");
+		calcu.mostrarResultado();
 
 	}
 
